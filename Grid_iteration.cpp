a2005609273a994b8242aaa7b96d1f80a430b124
Haifeng Yang <hfyangpku@gmail.com>
2017-05-25 15:55:37 -0400

    Skeleton of iterration method.

---
+++ Grid_iteration.cpp
@@ -0,0 +1,21 @@
+#include "Grid.h"
+
+void Grid::iteration(){
+  double r0, theta0;
+  double n_phi, n_theta;
+  for(int i=0; i<Nr; i++){ // i is index for radius in spacial grid
+    r0 = rc[i];
+    for(int j=0; j<Ntheta; j++){ // j is index for theta in spacial grid
+      theta0 = thetac[j];
+      for(int k=0; k<NphiI; k++){ // k is index for phi in angular grid
+        n_phi=phiIc[k];
+	for(int l=0; l<NthetaI; l++){ // l is index for theta in angular grid
+	  n_theta=thetaIc[l];
+
+	  // The following the the main body of calculation.
+
+	}
+      }
+    }
+  }
+}
