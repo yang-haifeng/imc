066ac1f3eaca338509dac243d89e9ee99f1ecfb0
Haifeng Yang <hfyangpku@gmail.com>
2017-06-05 23:26:38 -0400

    Slab model.

--- Grid_init.cpp
+++ Grid_init.cpp
@@ -1,12 +1,30 @@
 #include "Grid.h"
+#include <iostream>
 
+// Uniform Sphere Model
 void Grid::initDensity(){
   for (int i=0;i<Nr;i++){
     for (int j=0;j<Ntheta;j++){
-      Density[i*Ntheta+j] = 1.e-14;
+      Density[i*Ntheta+j] = 1.e-18;
+    }
+  }
+} 
+
+/* 
+// Uniform finite angular size Model
+void Grid::initDensity(){
+  double theta0 = PI/18.; // Total openning angle is 20 degree;
+  for (int i=0;i<Nr;i++){
+    for (int j=0;j<Ntheta;j++){
+      if (thetac[j] > PI/2-theta0 && thetac[j] < PI/2+theta0)
+      {
+        Density[i*Ntheta+j] = 1.e-14;
+      }
+      else Density[i*Ntheta+j] = 1.e-18;
     }
   }
 }
+*/
 
 void Grid::initBnuT(){
   for (int i=0;i<Nr;i++){
