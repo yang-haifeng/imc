10c1ef3e3e42dd189bb442c36b67cb6707da18a3
Haifeng Yang <hfyangpku@gmail.com>
2017-06-12 11:59:22 -0400

    product function fixed.

--- Grid_muller_Matrix_AlignES.cpp
+++ Grid_muller_Matrix_AlignES.cpp
@@ -102,5 +102,11 @@ void Grid::muller_Matrix(double theta, double phi, double nx, double ny, double 
 }
 
 double product(double e1[3], double M[3][3], double e2[3]){
-  return 0.;
+  double s = 0;
+  for (int i=0; i<3; i++){
+    for (int j=0; j<3; j++){
+      s += e1[i] * M[i][j] * e2[j];
+    }
+  }
+  return s;
 }
