175d1f902a4df3f6b46906a2f64710a6daa65e4d
Haifeng Yang <hfyangpku@gmail.com>
2017-06-12 23:47:41 -0400

    Donot use namespace std.

--- utils.h
+++ utils.h
@@ -2,11 +2,10 @@
 #define _UTILS_H
 
 #include "math.h"
+#include <array>
 
-#define Matrix array <double, 16>
-#define Vector array <double, 4>
-
-using namespace std;
+#define Matrix std::array <double, 16>
+#define Vector std::array <double, 4>
 
 double dot(double * a, double * b);
 Matrix operator*(Matrix a, Matrix b);
