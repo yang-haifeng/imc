7b97e74ebce75e6658e941e97184dd3ce4f6b9ca
Haifeng Yang <hfyangpku@gmail.com>
2017-06-17 13:58:57 -0400

    loadStokes fixed.

--- Grid_saveload.cpp
+++ Grid_saveload.cpp
@@ -1,16 +1,17 @@
 #include "Grid.h"
-#include <cstdlib>
 #include <fstream>
 
 void Grid::saveStokes(std::string fName){
-  std::ofstream Fout;
+  std::ofstream Fout, Fout2;
   Fout.open(fName.c_str(), std::ios::binary);
+  double temp;
   for(int i=0; i<Nr; i++){
     for(int j=0; j<Ntheta; j++){
       for(int k=0; k<NphiI; k++){
         for(int l=0; l<NthetaI; l++){
 	  for (int m=0; m<4; m++){
-	    Fout.write( (char *) &Stokes[i*Ntheta*NphiI*NthetaI*4 + j * NphiI*NthetaI*4 + k*NthetaI*4 + l*4 +m], sizeof(double));
+	    temp = Stokes[i*Ntheta*NphiI*NthetaI*4 + j * NphiI*NthetaI*4 + k*NthetaI*4 + l*4 +m];
+	    Fout.write( reinterpret_cast<const char*>(&temp), sizeof(double));
 	  }
 	}
       }
@@ -22,14 +23,14 @@ void Grid::saveStokes(std::string fName){
 void Grid::loadStokes(std::string fName){
   std::ifstream Fin;
   Fin.open(fName.c_str(), std::ios::binary);
-  char buff[128];
+  double temp;
   for(int i=0; i<Nr; i++){
     for(int j=0; j<Ntheta; j++){
       for(int k=0; k<NphiI; k++){
         for(int l=0; l<NthetaI; l++){
 	  for (int m=0; m<4; m++){
-	    Fin.read(buff, sizeof(double));
-	    Stokes[i*Ntheta*NphiI*NthetaI*4 + j * NphiI*NthetaI*4 + k*NthetaI*4 + l*4 +m] = atof(buff);
+	  Fin.read(reinterpret_cast<char*>(&temp), sizeof(double));
+	  Stokes[i*Ntheta*NphiI*NthetaI*4 + j * NphiI*NthetaI*4 + k*NthetaI*4 + l*4 +m] = temp;
 	  }
 	}
       }
